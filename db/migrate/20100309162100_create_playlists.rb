a90751b994bd041cda975822754b23dcc6dff4fb
Andrey <andrey@andrey-desktop.(none)>
2010-03-09 22:32:30 +0600

    closes #82

---
+++ db/migrate/20100309162100_create_playlists.rb
@@ -0,0 +1,20 @@
+class CreatePlaylists < ActiveRecord::Migration
+  def self.up
+    create_table :playlists do |t|
+      t.string    :title
+      t.text      :description
+      t.string    :icon_file_name
+      t.string    :icon_content_type
+      t.integer   :icon_file_size
+      t.datetime  :icon_updated_at
+      t.integer   :user_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :playlists
+  end
+end
+
---
+++ db/migrate/20100309162957_create_tracks.rb
@@ -0,0 +1,18 @@
+class CreateTracks < ActiveRecord::Migration
+  def self.up
+    create_table :tracks do |t|
+      t.string  :title
+      t.string  :author
+      t.integer :bitrate
+      t.integer :dimension
+      t.integer :playlist_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :tracks
+  end
+end
+
