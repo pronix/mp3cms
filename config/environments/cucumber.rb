7f1cdb9931014b274b0f987b55beb286e9ad2da4
Andrey <andrey@andrey-desktop.(none)>
2010-03-09 21:30:48 +0600

    closes #76

--- config/environments/cucumber.rb
+++ config/environments/cucumber.rb
@@ -27,4 +27,5 @@ config.gem 'webrat',         :lib => false, :version => '>=0.3.0' unless File.di
 config.gem 'rspec',            :lib => false, :version => '>=1.3.0' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec'))
 config.gem 'rspec-rails',      :lib => false, :version => '>=1.3.2' unless File.directory?(File.join(Rails.root, 'vendor/plugins/rspec-rails'))
 
-config.gem "thoughtbot-factory_girl", :lib => "factory_girl"
+config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+
