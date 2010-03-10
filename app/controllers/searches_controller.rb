69d9ce227e864fceddb1623bebea8ff6bb106c0d
Diman <devandart@ya.ru>
2010-03-10 16:34:13 +0300

    Добавил модель logsearch, теперь мы знаем, что искали пользователи

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -8,21 +8,27 @@ class SearchesController < ApplicationController
     case params[:attribute]
       when "author"
         @rez_search = Track.search :conditions => { :author => params[:search] }
+        Lastsearch.create(:search_string => params[:search], :site_attributes => "author", :site_section => "mp3")
       when "title"
         @rez_search = Track.search :conditions => { :title => params[:search] }
+        Lastsearch.create(:search_string => params[:search], :site_attributes => "title", :site_section => "mp3")
       when "everywhere"
         @rez_search = Track.search params[:search]
+        Lastsearch.create(:search_string => params[:search], :site_attributes => "everywhere", :site_section => "mp3")
       else
         @rez_search = Track.search params[:search]
+        Lastsearch.create(:search_string => params[:search], :site_attributes => "everywhere", :site_section => "mp3")
     end
   end
 
   def playlists
     @rez_search = Playlist.search params[:search]
+    Lastsearch.create(:search_string => params[:search], :site_section => "playlist")
   end
 
   def news
     @rez_search = News.search params[:search]
+    Lastsearch.create(:search_string => params[:search], :site_section => "news")
   end
 
 end
---
+++ features/step_definitions/news_step.rb
@@ -0,0 +1,10 @@
+Допустим /^в проекте есть следующие новости$/ do |table|
+  hash = table.hashes()
+  hash.each {|i|
+    News.create(
+      :header => i[:header],
+      :text => i[:text]
+    )
+  }
+end
+
