bb94bfa5012e2c1617ef68d374fccbd853473534
Maxim <parallel588@gmail.com>
2010-03-11 00:40:23 +0500

    Добавление роли "user" по умолчанию

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,20 +7,29 @@ class User < ActiveRecord::Base
 
   # Associations
   belongs_to :referrer, :class_name => "User"
-  has_many :user_roles, :dependent => :destroy
-  has_many :roles, :through => :user_roles
+  has_and_belongs_to_many :roles
+  # has_many :roles_users, :dependent => :destroy
+  # has_many :roles, :through => :roles_users
 
   # Validations
   validates_format_of :webmoney_purse, :with => /^Z[0-9]{12}/, :allow_nil => true, :allow_blank => true
   validates_format_of :icq, :with => /\d+/, :allow_nil => true, :allow_blank => true
+  validates_presence_of :login
+
+  # callback
+  before_create :add_default_role
 
   # named_scope
   named_scope :bans, :conditions => { :ban => true }
   named_scope :active, :conditions => {:active => true}
   named_scope :inactive, :conditions => {:active => false}
 
-  attr_accessible :login, :email, :password, :password_confirmation, :icq, :webmobey_purse
+  attr_accessible :login, :email, :password, :password_confirmation, :icq, :webmobey_purse, :captcha_challenge
 
+
+  def add_default_role
+    add_role(:user)
+  end
 
   def signup!(params)
     self.login                 = params[:user][:login]
@@ -29,6 +38,8 @@ class User < ActiveRecord::Base
     self.password_confirmation = params[:user][:password_confirmation]
     self.icq                   = params[:user][:icq]
     self.webmoney_purse        = params[:user][:webmoney_purse]
+    self.captcha_solution      = params[:user][:captcha_solution]
+    self.captcha_challenge     = params[:user][:captcha_challenge]
     save_without_session_maintenance
   end
 
@@ -53,5 +64,23 @@ class User < ActiveRecord::Base
     reset_perishable_token!
     Notifier.deliver_password_reset_instructions(self)
   end
+
+  # roles
+  def has_role?(role)
+    self.roles.count(:conditions => ["name = ?", role.to_s]) > 0
+  end
+
+  # user.add_role("admin")
+  def add_role(role)
+    return if self.has_role?(role)
+    self.roles << Role.find_by_name(role.to_s)
+  end
+
+  def remove_role(role)
+    return false unless self.has_role?(role)
+    role = Role.find_by_name(role)
+    self.roles.delete(role)
+  end
+
 end
 
