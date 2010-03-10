a27a77dd02f1112723d90671626c5ec7e8dae1b1
Maxim <parallel588@gmail.com>
2010-03-10 16:24:51 +0500

    Регистрация пользователя и отпавка уведомлений на почту

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
     c.login_field = 'email'
-    c.validates_length_of_password_field_options = {:minimum => 6, :on => :update, :if => :has_no_credentials? }
-    c.validates_length_of_password_confirmation_field_options = {:on => :update, :minimum => 4, :if => :has_no_credentials?}
+    # c.validates_length_of_password_field_options = {:minimum => 6, :on => :update, :if => :has_no_credentials? }
+    # c.validates_length_of_password_confirmation_field_options = {:on => :update, :minimum => 4, :if => :has_no_credentials?}
   end
 
   # Associations
@@ -11,6 +11,8 @@ class User < ActiveRecord::Base
   has_many :roles, :through => :user_roles
 
   # Validations
+  validates_format_of :webmoney_purse, :with => /^Z[0-9]{12}/, :allow_nil => true, :allow_blank => true
+  validates_format_of :icq, :with => /\d+/, :allow_nil => true, :allow_blank => true
 
   # named_scope
   named_scope :bans, :conditions => { :ban => true }
@@ -19,4 +21,37 @@ class User < ActiveRecord::Base
 
   attr_accessible :login, :email, :password, :password_confirmation, :icq, :webmobey_purse
 
+
+  def signup!(params)
+    self.login                 = params[:user][:login]
+    self.email                 = params[:user][:email]
+    self.password              = params[:user][:password]
+    self.password_confirmation = params[:user][:password_confirmation]
+    self.icq                   = params[:user][:icq]
+    self.webmoney_purse        = params[:user][:webmoney_purse]
+    save_without_session_maintenance
+  end
+
+  def activate!
+    self.active = true
+    save
+  end
+
+  # deliver
+  def deliver_activation_instructions!
+    reset_perishable_token!
+    Notifier.deliver_activation_instructions(self)
+  end
+
+  def deliver_activation_confirmation!
+    reset_perishable_token!
+    Notifier.deliver_activation_confirmation(self)
+  end
+
+
+  def deliver_password_reset_instructions!
+    reset_perishable_token!
+    Notifier.deliver_password_reset_instructions(self)
+  end
 end
+
