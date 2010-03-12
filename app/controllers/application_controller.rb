7e8667ee67c1dc86f2b0da8a30ef370c0322fb3c
Maxim <parallel588@gmail.com>
2010-03-12 19:35:19 +0500

    Установка пользователя для авторизации

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   helper_method :current_user_session, :current_user
   filter_parameter_logging :password, :password_confirmation
+  before_filter :set_current_user
+
 
 
   private
+  def set_current_user
+    Authorization.current_user = current_user || nil
+  end
+  def permission_denied
+    flash[:error] = "Sorry, you are not allowed to access that page."
+    redirect_to root_url
+  end
+
   def current_user_session
     return @current_user_session if defined?(@current_user_session)
     @current_user_session = UserSession.find
