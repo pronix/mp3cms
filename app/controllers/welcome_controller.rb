15c1e44248dc67513e3322ed1af8e32ea12e3c59
Diman <devandart@ya.ru>
2010-03-12 20:19:59 +0300

    Полтавлен лимит 10 на вывод последних запросов поиска и облогорожен внешний вид

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
 
   def index
-    @lastrequests = Lastsearch.find(:all, :order => "created_at DESC")
+    @lastrequests = Lastsearch.find(:all, :order => "created_at DESC", :limit => 10)
   end
 
 end
