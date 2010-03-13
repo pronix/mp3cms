5638454ac504837c8983e4769e05d3690f757927
Diman <devandart@ya.ru>
2010-03-13 03:53:14 +0300

    Добавленны заголовки для поиска

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -19,16 +19,36 @@ class SearchesController < ApplicationController
         @rez_search = Track.search params[:search_mp3]
         Lastsearch.create(:search_string => params[:search_mp3], :site_attributes => "everywhere", :site_section => "mp3")
     end
+
+    unless @rez_search.empty?
+      @title = params[:search_mp3]
+    else
+      @title = "Фаил не найден в нашей базе попробуйте запросить его в столе заказов"
+    end
+
   end
 
   def playlists
     @rez_search = Playlist.search params[:search_playlist]
     Lastsearch.create(:search_string => params[:search_playlist], :site_section => "playlist")
+
+    unless @rez_search.empty?
+      @title = params[:search_playlist]
+    else
+      @title = "Новостей с схожих с запросом не найденно"
+    end
+
   end
 
   def news
     @rez_search = NewsItem.search params[:search_news]
     Lastsearch.create(:search_string => params[:search_news], :site_section => "news")
+    unless @rez_search.empty?
+      @title = params[:search_news]
+    else
+      @title = "Новостей с схожих с запросом не найденно"
+    end
+
   end
 
 end
