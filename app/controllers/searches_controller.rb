e2d36935bd6f0ced07d5a32edefe456383f0fc22
Diman <devandart@ya.ru>
2010-03-13 04:29:41 +0300

    Добавил титлы запросов к поиску, и фичи. Всё ок при всех вариантах запросов и вариантах ответов.

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,48 +5,61 @@ class SearchesController < ApplicationController
   end
 
   def mp3
-    case params[:attribute]
-      when "author"
-        @rez_search = Track.search :conditions => { :author => params[:search_mp3] }
-        Lastsearch.create(:search_string => params[:search_mp3], :site_attributes => "author", :site_section => "mp3")
-      when "title"
-        @rez_search = Track.search :conditions => { :title => params[:search_mp3] }
-        Lastsearch.create(:search_string => params[:search_mp3], :site_attributes => "title", :site_section => "mp3")
-      when "everywhere"
-        @rez_search = Track.search params[:search_mp3]
-        Lastsearch.create(:search_string => params[:search_mp3], :site_attributes => "everywhere", :site_section => "mp3")
-      else
-        @rez_search = Track.search params[:search_mp3]
-        Lastsearch.create(:search_string => params[:search_mp3], :site_attributes => "everywhere", :site_section => "mp3")
-    end
+    if params[:search_mp3].size > 0
+      case params[:attribute]
+        when "author"
+          @rez_search = Track.search :conditions => { :author => params[:search_mp3] }
+          Lastsearch.create(:search_string => params[:search_mp3], :site_attributes => "author", :site_section => "mp3")
+        when "title"
+          @rez_search = Track.search :conditions => { :title => params[:search_mp3] }
+          Lastsearch.create(:search_string => params[:search_mp3], :site_attributes => "title", :site_section => "mp3")
+        when "everywhere"
+          @rez_search = Track.search params[:search_mp3]
+          Lastsearch.create(:search_string => params[:search_mp3], :site_attributes => "everywhere", :site_section => "mp3")
+        else
+          @rez_search = Track.search params[:search_mp3]
+          Lastsearch.create(:search_string => params[:search_mp3], :site_attributes => "everywhere", :site_section => "mp3")
+      end
 
-    unless @rez_search.empty?
-      @title = params[:search_mp3]
+      unless @rez_search.empty?
+        @title = params[:search_mp3]
+      else
+        @title = "Фаил не найден в нашей базе попробуйте запросить его в столе заказов"
+      end
+
     else
-      @title = "Фаил не найден в нашей базе попробуйте запросить его в столе заказов"
+      @title = "У вас пустой запрос"
     end
 
   end
 
   def playlists
-    @rez_search = Playlist.search params[:search_playlist]
-    Lastsearch.create(:search_string => params[:search_playlist], :site_section => "playlist")
+    if params[:search_playlist].size > 0
+      @rez_search = Playlist.search params[:search_playlist]
+      Lastsearch.create(:search_string => params[:search_playlist], :site_section => "playlist")
 
-    unless @rez_search.empty?
-      @title = params[:search_playlist]
+      unless @rez_search.empty?
+        @title = params[:search_playlist]
+      else
+        @title = "Плейлистов схожих с запросом не найденно"
+      end
     else
-      @title = "Плейлистов схожих с запросом не найденно"
+      @title = "У вас пустой запрос"
     end
 
   end
 
   def news
-    @rez_search = NewsItem.search params[:search_news]
-    Lastsearch.create(:search_string => params[:search_news], :site_section => "news")
-    unless @rez_search.empty?
-      @title = params[:search_news]
+    if params[:search_news].size > 0
+      @rez_search = NewsItem.search params[:search_news]
+      Lastsearch.create(:search_string => params[:search_news], :site_section => "news")
+      unless @rez_search.empty?
+        @title = params[:search_news]
+      else
+        @title = "Новостей с схожих с запросом не найденно"
+      end
     else
-      @title = "Новостей с схожих с запросом не найденно"
+      @title = "У вас пустой запрос"
     end
 
   end
