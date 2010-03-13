c473f05f9713cb8b0e19c78bc7ff5d68fbc8f136
Maxim <parallel588@gmail.com>
2010-03-13 12:56:59 +0500

    Вывод сообщений о нарушении прав доступа

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,15 +9,16 @@ class ApplicationController < ActionController::Base
   before_filter :set_current_user
 
 
+  def permission_denied
+    flash[:error] = "Sorry, you are not allowed to access that page."
+    redirect_to root_path
+  end
 
   private
   def set_current_user
-    Authorization.current_user = current_user || nil
+    Authorization.current_user = current_user
   end
-  def permission_denied
-    flash[:error] = "Sorry, you are not allowed to access that page."
-    redirect_to root_url
-  end
+
 
   def current_user_session
     return @current_user_session if defined?(@current_user_session)
