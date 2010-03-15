08f3a39ff368816473c889b5e81571cbb2638c12
Maxim <parallel588@gmail.com>
2010-03-15 10:03:08 +0500

    Добавил все возможные роли в фикстуры

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -26,6 +26,23 @@ Factory.define :user_requesting_password, :parent=>:user do |u|
   u.perishable_token  "abcd"
 end
 
+
+
+# :admin,                     # Доступ в админку - да/нет
+# :add_news_with_moderation,  # Добавление новостей с предмодерацией - да/нет
+# :upload_on_ftp,             # Аплоад на фтп сервер - да/нет
+# :add_mp3,                   # Добавление мп3 на сайт - да/нет
+# :moderation_all_mp3,        # Модерация всех mp3 - да/нет
+# :moderation_your_mp3,       # Модерация своих mp3 - да/нет
+# :free_download,             # Бесплатное скачивание мп3 - да/нет
+# :captcha_before_download,   # Капча перед скачиванием - да/нет
+# :award_points,              # Начислять баллы за действия - да/нет
+# :assorted_mp3,              # Разрешить мод нарезки файла - да/нет
+# :playlist,                  # Разрешить плейлисты - да/нет
+# :comment,                   # Разрешить комментарии - да/нет
+# :captcha_before_comment,    # Капча для камента - да/нет
+
+
 Factory.define :role do |r|
 end
 
@@ -52,3 +69,15 @@ Factory.define :moderator_role, :parent => :role do |r|
   r.description "moderator"
 end
 
+[ :add_news_with_moderation,  :upload_on_ftp, :add_mp3, :moderation_all_mp3,
+  :moderation_your_mp3, :free_download, :captcha_before_download,
+  :award_points, :assorted_mp3, :playlist, :comment, :captcha_before_comment].each do |m|
+  Factory.define "custom_#{m}_role", :parent => :role do |r|
+    r.name "#{m}"
+    r.title "#{m}"
+    r.system false
+    r.admin false
+    r.description "#{m}"
+    r.add_attribute m, true
+  end
+end
