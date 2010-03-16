058e108faafd05199ff4b54d5c70999b4d5af742
Diman <devandart@ya.ru>
2010-03-16 20:58:01 +0300

    Теперь у новостей есть текстовый редактор с возможностью, лёгкого аплода картинок

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,26 @@
-class Picture < ActiveRecord::Base
+class Picture < Asset
   belongs_to :imageable, :polymorphic => true
+
+  has_attached_file :data,
+                      :url  => "/assets/pictures/:id/:style_:basename.:extension",
+                      :path => ":rails_root/public/assets/pictures/:id/:style_:basename.:extension",
+	              :styles => { :content => '575>', :thumb => '100x100' }
+  validates_attachment_size :data, :less_than => 2.megabytes
+
+  def url_content
+    url(:content)
+  end
+
+  def url_thumb
+    url(:thumb)
+  end
+
+  def to_json(options = {})
+    options[:methods] ||= []
+    options[:methods] << :url_content
+    options[:methods] << :url_thumb
+    super options
+  end
+
 end
 
