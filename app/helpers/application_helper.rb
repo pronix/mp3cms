24b6df78a4ccfa2a69b6b3f2040305b0267ec3b3
Maxim <parallel588@gmail.com>
2010-03-16 16:11:55 +0500

    Добавил перезугрузку link_to чтоб в сылке указывался текущий пользователь

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,21 @@ module ApplicationHelper
       str
     end
   end
+
+  # Перегрузка ссылки, в ссылку добавляеться текущий пользователь
+  # def link_to(*args, &block)
+  #   options = []
+  #   options << args.first
+  #   options << (current_user ? (
+  #                               args.second['?'] ? args.second << "&u=#{current_user.id}" :
+  #                               args.second << "?u=#{current_user.id}"
+  #                               ) : args.second)
+  #   options << args.third
+  #   if block_given?
+  #     super(*options, &block)
+  #   else
+  #     super(*options)
+  #   end
+  # end
+
 end
