2dea38191fb9202655d4121239c4ecf26ffd57c7
Diman <devandart@ya.ru>
2010-03-16 03:57:52 +0300

    Поправил степ на добавление новостей и добавил новый на добавление категорий

--- features/step_definitions/news_step.rb
+++ features/step_definitions/news_step.rb
@@ -1,13 +1,18 @@
-Допустим /^в сервисе есть следующие новости$/ do |table|
+
+Допустим /^в категории "([^\"]*)" есть следующие новости$/ do |name, table|
+  category = NewsCategory.find_by_name(name)
   hash = table.hashes()
   hash.each {|i|
-    NewsItem.create(
+    NewsItem.create!(
+      :id => i[:id],
       :header => i[:header],
-      :text => i[:text]
+      :meta => i[:meta],
+      :text => i[:text],
+      :news_category_ids => [category.id]
     )
+
   }
 end
-
 
 Допустим /^новостей нет$/ do
   NewsItem.delete_all
