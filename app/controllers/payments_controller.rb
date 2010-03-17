d32d1818d94c878d3fee73154566e8adfe73155b
Maxim <parallel588@gmail.com>
2010-03-17 10:44:32 +0500

    Вывод транзакции пользователю

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -8,12 +8,12 @@
 
 class PaymentsController < ApplicationController
   before_filter :require_user
-  # filter_access_to :all, :attribute_check => false
+  filter_access_to :all, :attribute_check => false
 
   def show
     @kind_transactions = { }
     Profit.all.map{ |x| @kind_transactions[x.code] = x.name }
-    @credits = current_user.transactions.credits
-    @debits = current_user.transactions.debits
+    @credits = current_user.transactions.credits.group_by { |x| x.date_transaction.beginning_of_day }
+    @debits  = current_user.transactions.debits.group_by  { |x| x.date_transaction.beginning_of_day }
   end
 end
