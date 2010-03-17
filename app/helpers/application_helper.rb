efdfe73403dd4efc117608d45d61a22f533f75c6
Maxim <parallel588@gmail.com>
2010-03-17 23:06:18 +0500

    fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,19 +9,6 @@ module ApplicationHelper
 
   # Привлекать пользователя можно ссылаясь на главную, на страницу песни, исполнителя, результат поиска, или плейлист.
   # Перегрузка ссылки, в ссылку добавляеться текущий пользователь
-  def link_to(*args, &block)
-    options = []
-    options << args.first
-    options << ((current_user && args.second[/welcome|playlist|track|search|news/] &&
-                 !args.second["u=#{current_user.id}"] ) ? (
-                                                           args.second['?'] ? args.second << "&u=#{current_user.id}" :
-                                                           args.second << "?u=#{current_user.id}"
-                                                           ) : args.second)
-    options << args.third
-    block_given? ? super(*options, &block) :  super(*options)
-  end
-
-  # Прямое указание ссылки с параметром пользователя
   def link_to_with_referrer(*args, &block)
     options = []
     options << args.first
