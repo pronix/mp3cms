6725ae5799132d8364d59438b4393e89bc35fe21
Diman <devandart@ya.ru>
2010-03-17 11:55:36 +0300

    Добавил последние новости на главную страницу

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,7 @@ class WelcomeController < ApplicationController
 
   def index
     @lastrequests = Lastsearch.find(:all, :order => "created_at DESC", :limit => 10)
+    @last_news_items = NewsItem.find(:all, :order => "created_at DESC", :limit => 6)
   end
 
 end
---
+++ app/models/super_search.rb
@@ -0,0 +1,6 @@
+class SuperSearch
+  def search(serch_string, name_model, attribute)
+
+  end
+end
+
