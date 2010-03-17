130ee4c97b00beacdf46951e8423fa440a419d20
Maxim <parallel588@gmail.com>
2010-03-17 23:02:47 +0500

    Перегрузка хелпера link_to для указания referrer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,20 +6,31 @@ module ApplicationHelper
     end
   end
 
+
+  # Привлекать пользователя можно ссылаясь на главную, на страницу песни, исполнителя, результат поиска, или плейлист.
   # Перегрузка ссылки, в ссылку добавляеться текущий пользователь
-  # def link_to(*args, &block)
-  #   options = []
-  #   options << args.first
-  #   options << (current_user ? (
-  #                               args.second['?'] ? args.second << "&u=#{current_user.id}" :
-  #                               args.second << "?u=#{current_user.id}"
-  #                               ) : args.second)
-  #   options << args.third
-  #   if block_given?
-  #     super(*options, &block)
-  #   else
-  #     super(*options)
-  #   end
-  # end
+  def link_to(*args, &block)
+    options = []
+    options << args.first
+    options << ((current_user && args.second[/welcome|playlist|track|search|news/] &&
+                 !args.second["u=#{current_user.id}"] ) ? (
+                                                           args.second['?'] ? args.second << "&u=#{current_user.id}" :
+                                                           args.second << "?u=#{current_user.id}"
+                                                           ) : args.second)
+    options << args.third
+    block_given? ? super(*options, &block) :  super(*options)
+  end
+
+  # Прямое указание ссылки с параметром пользователя
+  def link_to_with_referrer(*args, &block)
+    options = []
+    options << args.first
+    options << (current_user ? (
+                                args.second['?'] ? args.second << "&u=#{current_user.id}" :
+                                args.second << "?u=#{current_user.id}"
+                                ) : args.second)
+    options << args.third
+    block_given? ? link_to(*options, &block) : link_to(*options)
+  end
 
 end
