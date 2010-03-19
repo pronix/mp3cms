e449b241a8e23b13107de6bcd028480d23b4b6a5
Maxim <parallel588@gmail.com>
2010-03-19 12:38:50 +0500

    реализованы фичи на вебмани и заявку на выплаты пользователемя

--- features/step_definitions/payments_steps.rb
+++ features/step_definitions/payments_steps.rb
@@ -13,11 +13,44 @@ Given /^есть следующие транзакции в сервиса:$/ do
             )
   end
 end
-# When /^I delete the (\d+)(?:st|nd|rd|th) payments$/ do |pos|
-#   visit payments_url
-#   within("table tr:nth-child(#{pos.to_i+1})") do
-#     click_link "Destroy"
-#   end
-# end
 
+When /^(?:|я )не увижу ссылку "([^\"]*)"$/ do |link|
+  response.should_not have_tag("a",  link)
+end
 
+Given /^прописаны параметры платежного шлюза "([^\"]*)"$/ do |gateway|
+  Factory gateway.to_sym
+end
+
+Then /^увижу форму отправки на оплату через webmoney$/ do
+  response.should have_tag("form[action=?]",  Gateway.webmoney.url)
+end
+Then /^у пользователя "([^\"]*)" должна быть открытая транзакция$/ do |email_user|
+  user = User.find_by_email email_user
+  tr = user.transactions.open.last
+  tr.should_not be_nil
+  tr.kind_transaction.should == Transaction::REFILL_BALANCE_WEBMONEY
+end
+Then /^у пользователя "([^\"]*)" должна быть открытая заявка на вывод денег$/ do |email_user|
+  user = User.find_by_email email_user
+  tr =   tr = user.transactions.open.withdraws.last
+  tr.should_not be_nil
+  tr.kind_transaction.should == Transaction::WITHDRAW
+
+end
+
+When /^изменим дату заявки на вывод денег пользователя "([^\"]*)" на "([^\"]*)"$/ do |email_user, new_date |
+  user = User.find_by_email email_user
+  tr =   tr = user.transactions.open.withdraws.last
+  tr.should_not be_nil
+  tr.date_transaction = Time.parse new_date
+  tr.save!
+end
+Given /^транзакции в сервисе еще нет$/ do
+  Transaction.destroy_all
+end
+
+Then /^я увижу табличные данные по выплатам:$/ do |_table|
+  Then %(я увижу "Дата(.*)Тип операции(.*)Статус(.*)Сумма")
+  And %(я увижу "Заказ выплаты(.*)В обработке(.*)10.00 руб.")
+end
