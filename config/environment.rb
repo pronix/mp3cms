e4b2e74aa75f48356d4ffdbd8097d86d6e71d900
Andrey <andrey@andrey-desktop.(none)>
2010-03-20 19:03:32 +0600

    Автоопределение кодировки и логика управления тегами и вводимой информации при создании треков

--- config/environment.rb
+++ config/environment.rb
@@ -25,10 +25,10 @@ Rails::Initializer.run do |config|
   config.gem 'rubyist-aasm', :lib => 'aasm', :source => "http://gems.github.com"
   config.gem 'ruby-mp3info', :lib => 'mp3info', :source => 'http://gemcutter.org'
   config.gem 'delayed_job', :lib => 'delayed_job', :source => 'http://gemcutter.org'
+  config.gem 'chardet', :lib => 'UniversalDetector', :source => 'http://gemcutter.org'
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
   config.i18n.default_locale = :ru
-
 
   config.time_zone = 'UTC'
 end
--- config/initializers/string.rb
+++ config/initializers/string.rb
@@ -1,1 +1,22 @@
+require 'iconv'
+require 'UniversalDetector'
+
+class String
+
+  def to_utf8
+    target_encoding = "UTF-8"
+    begin
+      source_encoding = UniversalDetector::chardet(self)["encoding"].to_s
+      reencoded_string = Iconv.iconv(target_encoding, source_encoding, self).to_s
+      if target_encoding == source_encoding
+        return self
+      else
+        return reencoded_string
+      end
+    rescue
+      "---------"
+    end
+  end
+
+end
 
