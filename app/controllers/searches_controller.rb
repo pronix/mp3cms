0b0da6366db2cc896efbc16a2cf4154474cdab40
Diman <devandart@ya.ru>
2010-03-22 16:13:55 +0300

    Переименованы поля в таблице lastsearches

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,12 +4,27 @@ class SearchesController < ApplicationController
     case params[:model]
       when "playlist"
         @rez_search = Playlist.search_playlist(params, per_page = 10)
+
+        unless @rez_search.empty?
+          Lastsearch.add_search(params)
+        end
+
         @params = "playlist"
       when "track"
         @rez_search = Track.search_track(params, per_page = 10)
+
+        unless @rez_search.empty?
+          Lastsearch.add_search(params)
+        end
+
         @params = "track"
       when "news_item"
         @rez_search = NewsItem.search_newsitem(params, per_page = 10)
+
+        unless @rez_search.empty?
+          Lastsearch.add_search(params)
+        end
+
         @params = "news"
       else
         @rez_search = NewsItem.search_newsitem(params, :page => params)
--- app/models/lastsearch.rb
+++ app/models/lastsearch.rb
@@ -1,2 +1,23 @@
 class Lastsearch < ActiveRecord::Base
+
+  def self.add_search(query)
+    case query[:model]
+      when "track"
+        if query[:attribute] == "author" or query[:attribute] == "everywhere" or query[:attribute] == "title"
+          unless query[:search_track].empty?
+            self.create(:url_model => query[:model], :url_string => query[:search_track], :url_attributes => query[:attribute] )
+          end
+        end
+      when "playlist"
+        unless query[:search_playlist].empty?
+          self.create(:url_model => query[:model], :url_string => query[:search_playlist] )
+        end
+      when "news_item"
+        unless query[:search_news].empty?
+          self.create(:url_model => query[:model], :url_string => query[:search_news] )
+        end
+    end
+  end
+
 end
+
