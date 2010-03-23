d2536b2e2088128077d3a2c5827f3125969cb0b0
Diman <devandart@ya.ru>
2010-03-23 22:43:38 +0300

    Добавлена привюшка к новости и дескрипшен

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -10,6 +10,8 @@ class NewsItem < ActiveRecord::Base
   has_many :comments
   acts_as_commentable
 
+  has_attached_file :avatar, :styles => { :origin => "108x108>" }
+
   define_index do
     indexes header, :sortable => true
     indexes text
