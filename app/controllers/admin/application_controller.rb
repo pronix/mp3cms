8d1c0ba774b37804ef2a9bfd43df7de686ae109c
Maxim <parallel588@gmail.com>
2010-03-27 14:30:41 +0500

    верстка

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,8 +1,6 @@
 class Admin::ApplicationController < ApplicationController
   before_filter :require_user
-  helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  layout "admin"
+  layout "application"
 
   protected
 
