369053fea0a39d936347203245dcdc752cd42c76
Maxim <parallel588@gmail.com>
2010-03-28 12:00:59 +0600

    Добавил задачи для крона

--- config/deploy.rb
+++ config/deploy.rb
@@ -81,6 +81,56 @@ namespace :bluepill do
   end
 end
 
+set :sphinx_role, :app
+
+namespace :thinking_sphinx do
+
+  desc "Starts the thinking sphinx searchd server"
+  task :start, :roles => sphinx_role do
+    puts "Starting thinking sphinx searchd server"
+    run "cd #{current_path}; rake RAILS_ENV=production thinking_sphinx:configure"
+    run "cd #{current_path}; rake RAILS_ENV=production ts:start"
+  end
+
+  desc "Stops the thinking sphinx searchd server"
+  task :stop, :roles => sphinx_role do
+    puts "Stopping thinking sphinx searchd server"
+    run "cd #{current_path}; rake RAILS_ENV=production thinking_sphinx:configure"
+    run "cd #{current_path}; rake RAILS_ENV=production ts:stop"
+  end
+
+  desc "Restarts the thinking sphinx searchd server"
+  task :restart, :roles => sphinx_role do
+    thinking_sphinx.stop
+    thinking_sphinx.index
+    thinking_sphinx.start
+  end
+
+  desc "Copies the shared/config/sphinx yaml to release/config/"
+  task :symlink_config, :roles => :app do
+    run "ln -s #{shared_path}/config/sphinx.yml #{release_path}/config/sphinx.yml"
+  end
+
+  desc "Displays the thinking sphinx log from the server"
+    task :tail, :roles => :app do
+      stream "tail -f #{shared_path}/log/searchd.log"
+  end
+
+  desc "Runs Thinking Sphinx indexer"
+  task :index, :roles => sphinx_role do
+    puts "Updating search index"
+    run "cd #{current_path}; rake RAILS_ENV=production ts:index"
+  end
+end
+
+namespace :whenever do
+  desc "Update the crontab file"
+  task :update_crontab, :roles => :db do
+    run "cd #{release_path} && whenever --update-crontab #{application}"
+  end
+end
 
 
-after "deploy:update",  "deploy:symlinks", "deploy:chown",  "bluepill:quit", "bluepill:start", "deploy:restart_vsftpd"
+after "deploy:update",  "deploy:symlinks", "deploy:chown", "whenever:update_crontab", "bluepill:quit", "bluepill:start", "deploy:restart_vsftpd"
+# after "deploy:update_code", "thinking_sphinx:symlink_config" # sym thinking_sphinx.yml on update code
+after "deploy:restart"    , "thinking_sphinx:restart"     # restart thinking_sphinx on app restart
