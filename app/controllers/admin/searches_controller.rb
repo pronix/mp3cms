3a03edc34d13fed2020a7fd46e4d33c422affa46
Maxim <parallel588@gmail.com>
2010-03-29 18:19:51 +0600

    Примерная верстка для поиска по трекам в админке

--- app/controllers/admin/searches_controller.rb
+++ app/controllers/admin/searches_controller.rb
@@ -1,37 +1,25 @@
 class Admin::SearchesController < ApplicationController
-
   filter_access_to :all, :attribute_check => false
 
-  layout "admin"
 
 # default_behavior в в аргументе метода search говорит о том что мы вышли на страницу без дололнительных аргументов и по дефолту произойдёт выборка "список треков на модерации"
   def show
-    if params[:search_track] != ""
-      case params[:model]
-        when "track"
-          @rez_search = Track.search_track(params, per_page = 10)
-          @partial = "track"
-          @index = 0
-        when "playlist"
-          @rez_search = Playlist.search_playlist(params, per_page = 10)
-          @partial = "playlist"
-        when "news_item"
-          @rez_search = NewsItem.search_newsitem(params, per_page = 10)
-          @partial = "news_item"
-        when "user"
-          @rez_search = User.search_user(params, per_page = 10)
-          @partial = "user"
-        when "transaction"
-          @rez_search = Transaction.search_transaction(params, per_page = 10)
-          @partial = "transaction"
-        else
-          @rez_search = Track.search_track(params, per_page = 10)
-          @partial = "track"
-          @index = 0
-      end
+    @index = 0
+    @partial = (!params[:model].blank? && params[:model][/playlist|news_item|user|transaction/]) ? params[:model] : "track"
+    unless params[:search_track].blank?
+      @rez_search = case params[:model]
+                    when "playlist"    then Playlist.search_playlist(params, per_page = 10)
+                    when "news_item"   then NewsItem.search_newsitem(params, per_page = 10)
+                    when "user"        then User.search_user(params, per_page = 10)
+                    when "transaction" then Transaction.search_transaction(params, per_page = 10)
+                    else
+                      Track.search_track(params, per_page = 10)
+                    end
     else
+      @rez_search = nil
       flash[:notice] = "У вас пустой запрос"
     end
+
   end
 
 
