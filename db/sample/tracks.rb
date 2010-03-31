2643e798dd9a8c8440637195038f05992671ee20
Diman <devandart@ya.ru>
2010-03-31 21:04:40 +0400

    Поправленна вёрстка

--- db/sample/tracks.rb
+++ db/sample/tracks.rb
@@ -2,12 +2,12 @@ require "populator"
 
 [Track].each(&:delete_all)
 
-Track.populate 100 do |track|
-  track.title = Populator.words(2..4)
-  track.author = Populator.words(1..2)
-  track.bitrate = [128, 192, 168]
-  track.delta = 1
-  track.user_id = 1
-  track.state = "active"
-end
+#Track.populate 100 do |track|
+#  track.title = Populator.words(2..4)
+#  track.author = Populator.words(1..2)
+#  track.bitrate = [128, 192, 168]
+#  track.delta = 1
+#  track.user_id = 1
+#  track.state = "active"
+#end
 
