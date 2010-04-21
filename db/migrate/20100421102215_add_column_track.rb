c2c10204695641bdaa63885b677bb397ef18f761
Diman <devandart@ya.ru>
2010-04-21 21:33:11 +0400

    Добавленна миграция

---
+++ db/migrate/20100421102215_add_column_track.rb
@@ -0,0 +1,10 @@
+class AddColumnTrack < ActiveRecord::Migration
+  def self.up
+    add_column(:tracks, :length, :string)
+  end
+
+  def self.down
+    remove_column(:tracks, :length)
+  end
+end
+
