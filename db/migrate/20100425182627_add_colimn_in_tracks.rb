93af6947a58627d9060c5c725f41fd64a521184c
Diman <devandart@ya.ru>
2010-04-26 00:20:34 +0400

    Удалил лишнию миграцию

--- db/migrate/20100425182627_add_colimn_in_tracks.rb
+++
@@ -1,9 +0,0 @@
-class AddColimnInTracks < ActiveRecord::Migration
-  def self.up
-    add_column(:tracks, :length, :string)
-  end
-
-  def self.down
-    remove_column(:tracks, :length)
-  end
-end
