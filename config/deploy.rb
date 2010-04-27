ab1145524ac96b7793ca96482c1748744cba4a54
dima <pronix.service@gmail.com>
2010-04-27 18:24:04 +0400

    загруженые фотки в shared

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,7 @@ namespace :deploy do
   task :symlinks do
     run "mkdir -p #{shared_path}/data" unless File.exist?("#{shared_path}/data")
     run "ln -nfs #{shared_path}/data #{release_path}/data "
-    %w{assets playlists }.each do |share|
+    %w{assets playlists news_items system}.each do |share|
       run "mkdir -p #{shared_path}/public/#{share}" unless File.exist?("#{shared_path}/public/#{share}")
       run "ln -nfs #{shared_path}/public/#{share} #{release_path}/public/#{share} "
     end
