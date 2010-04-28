aa39a3a1a3856ce55672d3b28a54924e635c8b7b
dima <pronix.service@gmail.com>
2010-04-28 18:52:16 +0400

    fix autodeploy

--- app/models/satellite.rb
+++ app/models/satellite.rb
@@ -48,25 +48,8 @@ class SatelliteJob < Struct.new :id
   def perform
     sat = Satellite.find(id)
     ip = sat.ip
-    # ставим пакеты через yum
-    shr(ip,'yum install wget unzip make patch zlib-devel readline-devel zlib-devel gcc openssl-devel gcc-c++ sqlite-devel postgresql-devel git libuuid-devel libxml2-devel libxslt-devel net-snmp -y')
-    # ставим руби
-    puts shr(ip,'wget http://rubyforge.org/frs/download.php/68719/ruby-enterprise-1.8.7-2010.01.tar.gz ; tar -xzvf ./ruby-enterprise-1.8.7-2010.01.tar.gz')
-    # запускаем инсталлер
-    puts shr(ip,"cd ./ruby-enterprise-1.8.7-2010.01 ; ./installer -a / ; gem install rack passenger --no-ri --no-rdoc ; sed -i 's/download_and_install = should_we_download_and_install_nginx_automatically?/download_and_install = \"1\"/' /lib/ruby/gems/1.8/gems/passenger-2.2.11/bin/passenger-install-nginx-module ; sed -i 's/input.empty? || input.*/true/' /lib/ruby/gems/1.8/gems/passenger-2.2.11/bin/passenger-install-nginx-module ; sed -i '219,226d' /lib/ruby/gems/1.8/gems/passenger-2.2.11/bin/passenger-install-nginx-module ; sed -i 's/if prefix.empty?/if true/' /lib/ruby/gems/1.8/gems/passenger-2.2.11/bin/passenger-install-nginx-module")
-    # запустили установку nginx
-    shr(ip,'passenger-install-nginx-module --auto')
-    puts "настраиваем nginx"
-    system(" scp doc/satellite/nginx.conf root@#{sat.ip}:/etc/nginx.conf")
-    system(" scp doc/satellite/nginx root@#{sat.ip}:/etc/init.d/nginx")
-    puts "устанавливаем rack- приложение и служебные скрипты"
-    system(" ssh root@#{ip} 'mkdir -p /var/www/{data,public,tmp} ;'")
-    system("scp doc/satellite/config.ru root#{ip}:/var/www/")
-    system("ssh root@#{ip} 'chown -R nobody:nobody /var/www'")
-    # настраиваем запускем snmpd
-    system("scp doc/satellite/snmpd.conf root#{ip}:/etc/snmp/")
-    # запускаем
-    shr(ip,' server snmpd start ; service nginx start ; ')
+    system("scp -r doc/satelite/* root#{ip}:/root/")
+    system(" ssh root@#{ip} '/root/autodeploy.sh'")
     # тестируем
     # после успешной проверки ставим что сервер активен
     sat.active = true
---
+++ doc/satelite/autodeploy.sh
@@ -0,0 +1,22 @@
+yum install wget unzip make patch zlib-devel readline-devel zlib-devel gcc openssl-devel gcc-c++ sqlite-devel postgresql-devel git libuuid-devel libxml2-devel libxslt-devel net-snmp -y
+    # ставим руби
+wget http://rubyforge.org/frs/download.php/68719/ruby-enterprise-1.8.7-2010.01.tar.gz ; tar -xzvf ./ruby-enterprise-1.8.7-2010.01.tar.gz
+    # запускаем инсталлер
+cd ./ruby-enterprise-1.8.7-2010.01
+./installer -a /
+gem install rack passenger --no-ri --no-rdoc
+sed -i 's/download_and_install = should_we_download_and_install_nginx_automatically?/download_and_install = \"1\"/' /lib/ruby/gems/1.8/gems/passenger-2.2.11/bin/passenger-install-nginx-module
+sed -i 's/input.empty? || input.*/true/' /lib/ruby/gems/1.8/gems/passenger-2.2.11/bin/passenger-install-nginx-module
+sed -i '219,226d' /lib/ruby/gems/1.8/gems/passenger-2.2.11/bin/passenger-install-nginx-module
+sed -i 's/if prefix.empty?/if true/' /lib/ruby/gems/1.8/gems/passenger-2.2.11/bin/passenger-install-nginx-module
+# запустили установку nginx
+passenger-install-nginx-module --auto
+cp nginx.conf /etc/nginx.conf
+cp nginx /etc/init.d/nginx
+mkdir -p /var/www/{data,public,tmp}
+cp config.ru /var/www/
+chown -R nobody:nobody /var/www
+    # настраиваем запускем snmpd
+cp snmpd.conf root#{ip}:/etc/snmp/
+    # запускаем
+server snmpd start ; service nginx start
