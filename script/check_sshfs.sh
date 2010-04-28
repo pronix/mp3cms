0e87eeb9dc78d7cc56f7af5c31d6ca4c8ef4f362
dima <pronix.service@gmail.com>
2010-04-29 00:20:04 +0400

    bug fix

--- script/check_sshfs.sh
+++ script/check_sshfs.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-REZ=`./script/runner -e $RAILS_ENV "Satellite.get_servers('id_ip')"`
 
-for LINE in $REZ
+./script/runner -e $RAILS_ENV "Satellite.get_servers('id_ip')" | while read LINE
 do
-        ls data/tracks/`echo $LINE | cut -d" " -f1`
+        ls /var/www/mp3cms/shared/data/tracks/`echo $LINE | cut -d" " -f1`
         if [ $? == '0' ]
         then
-                fusermount -u data/tracks/`echo $LINE | cut -d" " -f1`
+                fusermount -u /var/www/mp3cms/shared/data/tracks/`echo $LINE | cut -d" " -f1`
                 sudo -u apache sshfs root@`echo $LINE | cut -d" " -f2`:/var/www/data /var/www/mp3cms/shared/data/tracks/`echo $LINE | cut -d" " -f1` -o umask=770
         fi
 done
