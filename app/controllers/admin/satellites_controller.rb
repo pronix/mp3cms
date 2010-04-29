d4d8bbf9fdc40f94231289cc46b3e8f1d474a7b9
dima <pronix.service@gmail.com>
2010-04-29 18:23:50 +0400

    назначение мастером локальный хост

--- app/controllers/admin/satellites_controller.rb
+++ app/controllers/admin/satellites_controller.rb
@@ -27,6 +27,7 @@ class Admin::SatellitesController < ApplicationController
   end
 
   def index
+    @local = Satellite.find_by_ip('127.0.0.1')
     @satellites = Satellite.find(:all, :conditions => ["ip != ?",'127.0.0.1'], :order => "created_at DESC")
     respond_to do |format|
       format.html{ }
