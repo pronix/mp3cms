e251f5578c163e37ed797ef795043d029c15ad06
dima <pronix.service@gmail.com>
2011-04-13 21:57:15 +0400

    fix captcha

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-#RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.11' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 
@@ -20,6 +20,7 @@ Rails::Initializer.run do |config|
   config.gem 'authlogic',  :source => 'http://gemcutter.org'
   config.gem 'paperclip',  :source => 'http://gemcutter.org', :version => '=2.2.9.2'
   config.gem "inherited_resources", :version => '=1.0.3'
+  config.gem 'rmagick', :version '<2.0.0'
   config.gem 'validates_captcha'
   config.gem "declarative_authorization", :source => "http://gemcutter.org"
   config.gem 'jackdempsey-acts_as_commentable', :lib => 'acts_as_commentable', :source => "http://gems.github.com"
