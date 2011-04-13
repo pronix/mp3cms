db8436457f1094e20e41d04723fa23c92d64d0e9
dima <pronix.service@gmail.com>
2011-04-13 22:23:28 +0400

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,7 +49,7 @@ namespace :deploy do
 
   task :chown, :roles => :app do
     run "chown -R apache:apache #{deploy_to}"
-    run  "chmod -R 777 /var/www/mp3cms/current/logs/search*"
+    run  "chmod -R 777 /var/www/mp3cms/current/log/search*"
   end
 
   desc "create symlinks on shared resources"
