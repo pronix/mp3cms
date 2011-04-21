c759adccfb6b09a5eb778d3fb32df38b63e3f75b
parallel588 <parallel588@gmail.com>
2011-04-21 08:37:01 +0600

    автоматический вход после активации учетной записи

--- app/controllers/activations_controller.rb
+++ app/controllers/activations_controller.rb
@@ -14,6 +14,7 @@ class ActivationsController < ApplicationController
 
     if @user.activate!
       @user.deliver_activation_confirmation!
+      UserSession.create(@user, true)
       flash[:notice] = "Ваш аккаунт был активорован."
       redirect_to root_path
     else
