a2a1d82d00fec3d9965e6708f92fead617072c17
parallel588 <parallel588@gmail.com>
2011-04-23 14:53:49 +0600

    Сообщения при создание заявки на вывод денег

--- app/controllers/withdraws_controller.rb
+++ app/controllers/withdraws_controller.rb
@@ -17,13 +17,17 @@ class WithdrawsController < ApplicationController
 
     if @amount && current_user.can_withdraw(@amount) && (@transaction = current_user.transactions.withdraw(@amount))
       flash[:notice] = "Заявка № #{@transaction.id} принята."
-      redirect_to payments_path :format => "js"
+      respond_to do |format|
+        format.html { redirect_to payments_path }
+        format.js   { render :text => flash[:notice] }
+      end
     else
-      flash[:amount_error] = unless current_user.errors.blank?
-                               ['<ul>',current_user.errors.full_messages.map{|x| "<li>#{x}</li>" }, '</ul>'].flatten.join
-                             else
-                               "Не правильная сумма."
-                             end
+      flash[:amount_error] =
+        unless current_user.errors.blank?
+          ['<ul>',current_user.errors.full_messages.map{|x| "<li>#{x}</li>" }, '</ul>'].flatten.join
+        else
+          "Не правильная сумма."
+        end
       respond_to do |format|
         format.html { render :action => :new }
         format.js { render :text => flash[:amount_error], :status => :internal_server_error }
