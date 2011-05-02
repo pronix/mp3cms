aa8db6af1001023da032f2233a3291c91743f5cc
parallel588 <parallel588@gmail.com>
2011-05-02 20:37:39 +0600

    ашч

--- app/controllers/mp3_cuts_controller.rb
+++ app/controllers/mp3_cuts_controller.rb
@@ -23,19 +23,8 @@ class Mp3CutsController < ApplicationController
     @track = Track.find_by_id params[:id]
     raise "Not found track" if @track.blank?
     raise "Не хватает денег" if current_user.balance < Profit.find_by_code("assorted_track").amount
-
-    # @time_range = [convert_seconds(params[:time][:start]), convert_seconds(params[:time][:stop])].flatten
-
-    # @word= Array.new(100){ ['A'..'Z'].map{ |r| r.to_a }.flatten[ rand( ['A'..'Z'].map{ |r| r.to_a }.flatten.size ) ] }.join
-    # @tmp_file = FileUtils.mkdir_p(CUT_PATH) &&
-    #   File.join(CUT_PATH, Digest::MD5.hexdigest([request.remote_ip, Time.now.to_i,@word].join))
-    # @command = "#{Settings[:mp3_cut_command]} -o #{@tmp_file} -t %02d:%02d:%02d+000-%02d:%02d:%02d+000 #{@track.data.path.gsub(' ','\ ')}"%@time_range
-
-    # `#{@command}`
-
-    @temp_file = Mp3Cut.cut(@track, params[:time][:start], params[:time][:stop])
+    @tmp_file = Mp3Cut.cut(@track, params[:time][:start], params[:time][:stop])
     current_user.debit_assorted_track("Нарезка трека")
-
     send_file  @tmp_file, :filename => @track.data_file_name, :content_type => 'application/mp3'
   rescue => ex
     flash[:error] = ex.message
