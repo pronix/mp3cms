b75586e90add85b914a0cff4d14475b2aabce20a
parallel588 <parallel588@gmail.com>
2011-05-23 16:45:20 +0600

    Настройик для unicorn в dev режиме

---
+++ config/unicorn.development.rb
@@ -0,0 +1,13 @@
+#encoding: utf-8
+
+
+ENV['RAILS_ENV'] = 'development'
+worker_processes 1
+listen 3000, :tcp_nopush => false
+timeout 30
+
+root = File.expand_path(File.dirname(__FILE__)).split('/')[0..-2].join('/')
+pid(root + '/tmp/pids/unicorn.pid')
+working_directory root
+stderr_path(root + '/log/unicorn.stderr.log')
+stdout_path(root + '/log/unicorn.stdout.log')
